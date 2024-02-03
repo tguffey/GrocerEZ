44c7e47faf3eff7f1612520c063ab94c139573e5
imcalledjocelyn <92833081+imcalledjocelyn@users.noreply.github.com>
2024-02-03 00:50:25 -0800

    changes for navbar color, themes, and logo

--- app/src/main/java/com/example/grocerez/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/grocerez/ui/home/HomeFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.grocerez.R
 import com.example.grocerez.databinding.FragmentHomeBinding
+import androidx.appcompat.widget.Toolbar
 
 class HomeFragment : Fragment() {
 
