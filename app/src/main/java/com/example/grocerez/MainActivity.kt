bafe5bb63ec60c0cc3ef3d09a727a2148f8f23dc
imcalledjocelyn <92833081+imcalledjocelyn@users.noreply.github.com>
2024-02-19 02:56:25 -0800

    i edited the Dark themes.xml

--- app/src/main/java/com/example/grocerez/MainActivity.kt
+++ app/src/main/java/com/example/grocerez/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.grocerez
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.TypedValue
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -20,20 +23,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-        //I added the line below to change the nav color to green001
-        navView.setBackgroundColor(resources.getColor(R.color.green001))
+
+        // I added a condition to check if the user is in dark or light mode
+        val isDarkMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+
+        // If user in dark mode the navbar will be green001 else it will be black
+        // I added this because before it was light green even for dark mode
+        if (isDarkMode) {
+            // Dark mode
+            navView.setBackgroundColor(resources.getColor(R.color.black))
+        } else {
+            // Light mode
+            navView.setBackgroundColor(resources.getColor(R.color.green001))
+        }
+        // end of code here. Jocelyn
 
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
+        val appBarConfiguration = AppBarConfiguration(            setOf(
                 R.id.navigation_myplate, R.id.navigation_dashboard, R.id.navigation_home,
                 R.id.navigation_recipes, R.id.navigation_shopping
             )
         )
-        //THIS IS JOCELYN IM DELETING THIS
+        //THIS IS JOCELYN Im deleting this to remove Action Bar bug
         //setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
