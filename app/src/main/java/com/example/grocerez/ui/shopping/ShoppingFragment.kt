1e9ba4b68bbbc9f1108bc19192463b0a017afc24
BrandNew8787 <80979158+BrandNew8787@users.noreply.github.com>
2024-02-19 13:06:24 -0800

    Merge front end with BrandonV1-1

--- app/src/main/java/com/example/grocerez/ui/shopping/ShoppingFragment.kt
+++ app/src/main/java/com/example/grocerez/ui/shopping/ShoppingFragment.kt
@@ -11,13 +11,15 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.grocerez.R
 import com.example.grocerez.databinding.FragmentShoppingBinding
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 class ShoppingFragment : Fragment() {
 
     private var _binding : FragmentShoppingBinding? = null
+    private lateinit var shoppingViewModel: ShoppingViewModel
+    // Tracks whether or not the edit options are expanded or not
     private var isExpanded = false
 
+    // Animation variables
     private lateinit var fabOpen: Animation
     private lateinit var fabClose: Animation
     private lateinit var fromBottomBg: Animation
@@ -27,55 +29,67 @@ class ShoppingFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    // Create the UI view
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val shoppingViewModel =
-            ViewModelProvider(this).get(ShoppingViewModel::class.java)
+        shoppingViewModel = ViewModelProvider(this).get(ShoppingViewModel::class.java)
 
         _binding = FragmentShoppingBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Label the shopping list as empty
         val textView: TextView = binding.textShopping
         shoppingViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
 
-        // animate the expandable Edit Item button
+        // Animate the expandable Edit Item button
         val context = requireContext()
         fabOpen = AnimationUtils.loadAnimation(context, R.anim.from_bottom_fab)
         fabClose = AnimationUtils.loadAnimation(context, R.anim.to_bottom_fab)
         fromBottomBg = AnimationUtils.loadAnimation(context, R.anim.from_bottom_anim)
         toBottomBg = AnimationUtils.loadAnimation(context, R.anim.to_bottom_anim)
 
+        // Determine whether to open or close edit options when the edit button is clicked
         binding.editItemFab.setOnClickListener {
             if (isExpanded) {
+                // Close the edit options if the options are expanded
                 shrinkFab()
             } else {
+                // Open the edit options if the options are not expanded
                 expandFab()
             }
         }
+        // Make a new sheet when the Add Item button is pressed
+        binding.addItemFab.setOnClickListener {
+            // Show New Grocery Item bottom dialog
+            NewGrocerySheet().show(parentFragmentManager, "newItemTag")
+        }
+
         return root
     }
 
+    // Destroy the view once the user navigates to a different page
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    //
+    // Close the edit option buttons
     private fun shrinkFab() {
-
         binding.transparentBg.startAnimation(toBottomBg)
         binding.clearListFab.startAnimation(fabClose)
         binding.addItemFab.startAnimation(fabClose)
         binding.addCategoryFab.startAnimation(fabClose)
 
+        // Toggle isExpanded
         isExpanded = !isExpanded
     }
 
+    // Open the edit option buttons
     private fun expandFab() {
 
         binding.transparentBg.startAnimation(fromBottomBg)
@@ -83,6 +97,7 @@ class ShoppingFragment : Fragment() {
         binding.addItemFab.startAnimation(fabOpen)
         binding.addCategoryFab.startAnimation(fabOpen)
 
+        // Toggle isExpanded
         isExpanded = !isExpanded
     }
 
