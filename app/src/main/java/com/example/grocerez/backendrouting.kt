dfd5bdc86a088b687df1e8b98f033f3513fe1bbc
Thong Chau <80946011+TChauFlamingHeart@users.noreply.github.com>
2024-02-19 21:25:11 -0800

    made a new branch with backend routing and connection to server

---
+++ app/src/main/java/com/example/grocerez/backendrouting.kt
@@ -0,0 +1,13 @@
+package com.example.grocerez
+
+// Address where remote nodeJS server is
+//val routeAddr = "http://3.19.53.211:3000"
+
+// Address for local nodeJS server is
+val routeAddr = "http://10.0.2.2:3000"
+
+// "http://10.0.2.2:3000" for local
+// "http://localhost:3000/" will NOT work
+// Our server "http://18.222.199.102:3000"
+// If you want to use your physical phone you could use your ip address plus :3000
+// This will allow your Android Emulator and physical device at your home to connect to the server
