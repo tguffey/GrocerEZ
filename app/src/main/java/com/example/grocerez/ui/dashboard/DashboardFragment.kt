fc3d305c7cb146eb135d4fe10bef9f8715f9766b
BrandNew8787 <80979158+BrandNew8787@users.noreply.github.com>
2024-03-04 19:06:08 -0800

    Added popup button to the Dashboard Fragment

--- app/src/main/java/com/example/grocerez/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/grocerez/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.grocerez.R
 import com.example.grocerez.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment(), FoodItemClickListener {
@@ -17,6 +20,13 @@ class DashboardFragment : Fragment(), FoodItemClickListener {
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var itemViewModel: DashboardViewModel
+    private var isExpanded = false
+
+    // Animation variables
+    private lateinit var fabOpen: Animation
+    private lateinit var fabClose: Animation
+    private lateinit var fromBottomBg: Animation
+    private lateinit var toBottomBg: Animation
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,10 +43,28 @@ class DashboardFragment : Fragment(), FoodItemClickListener {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        val context = requireContext()
+        fabOpen = AnimationUtils.loadAnimation(context, R.anim.from_bottom_fab)
+        fabClose = AnimationUtils.loadAnimation(context, R.anim.to_bottom_fab)
+        fromBottomBg = AnimationUtils.loadAnimation(context, R.anim.from_bottom_anim)
+        toBottomBg = AnimationUtils.loadAnimation(context, R.anim.to_bottom_anim)
+
+        // Determine whether to open or close edit options when the edit button is clicked
+        binding.editItemFab.setOnClickListener {
+            if (isExpanded) {
+                // Close the edit options if the options are expanded
+                shrinkFab()
+            } else {
+                // Open the edit options if the options are not expanded
+                expandFab()
+            }
+        }
+
         // Set OnClickListener for the newItemButton
-        binding.newItemButton.setOnClickListener {
+        binding.addItemFab.setOnClickListener {
             // Show the NewTaskSheet dialog
             NewTaskSheet(null).show(parentFragmentManager, "newItemTag")
+            shrinkFab()
         }
         setRecyclerView()
 
@@ -48,6 +76,29 @@ class DashboardFragment : Fragment(), FoodItemClickListener {
         super.onDestroyView()
         // Set the binding variable to null to avoid memory leaks
         _binding = null
+    }
+
+    // Close the edit option buttons
+    private fun shrinkFab() {
+        binding.transparentBg.startAnimation(toBottomBg)
+        binding.addCategoryFab.startAnimation(fabClose)
+        binding.addItemFab.startAnimation(fabClose)
+        binding.scanBarcode.startAnimation(fabClose)
+
+        // Toggle isExpanded
+        isExpanded = !isExpanded
+    }
+
+    // Open the edit option buttons
+    private fun expandFab() {
+
+        binding.transparentBg.startAnimation(fromBottomBg)
+        binding.addCategoryFab.startAnimation(fabOpen)
+        binding.addItemFab.startAnimation(fabOpen)
+        binding.scanBarcode.startAnimation(fabOpen)
+
+        // Toggle isExpanded
+        isExpanded = !isExpanded
     }
 
     private fun setRecyclerView() {
