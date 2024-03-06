090aee3de7be4a7295edba4d22e3f36e40f67dd3
Thong Chau <80946011+TChauFlamingHeart@users.noreply.github.com>
2024-03-05 23:21:09 -0800

    turned database into a singleton object

--- app/src/main/java/com/example/grocerez/Activity2.kt
+++ app/src/main/java/com/example/grocerez/Activity2.kt
@@ -3,6 +3,7 @@ package com.example.grocerez
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -35,5 +36,7 @@ class Activity2 : AppCompatActivity() {
         inputitemBtn.setOnClickListener {
 
         }
+
+        val displayText = findViewById<TextView>(R.id.displayText)
     }
 }
--- app/src/main/java/com/example/grocerez/AppDatabase.kt
+++ app/src/main/java/com/example/grocerez/AppDatabase.kt
@@ -1,10 +1,29 @@
 package com.example.grocerez
 
 // AppDatabase.kt
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun itemDao(): ItemDao
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "my_database.db")
+                            .build()
+                    }
+                }
+            }
+            return INSTANCE!!
+        }
+    }
 }
