8ef4b8dc6ecab7aafb77e437672314387e532e6c
Thong Chau <80946011+TChauFlamingHeart@users.noreply.github.com>
2024-03-05 22:48:07 -0800

    new metrict for items, it has id now

--- app/src/main/java/com/example/grocerez/Item.kt
+++ app/src/main/java/com/example/grocerez/Item.kt
@@ -2,9 +2,13 @@ package com.example.grocerez
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import androidx.room.ColumnInfo
+
+
+// for fundamental testing of database, item will only have name and category
 @Entity(tableName = "items")
 data class Item(
-    @PrimaryKey @ColumnInfo(name = "name") val name: String,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
     val category: String
 )
 
