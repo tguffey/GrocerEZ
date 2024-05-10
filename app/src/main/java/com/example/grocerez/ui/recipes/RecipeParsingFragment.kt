685e1d21ce35d317dd3521eaabefcfa5f8b7eb6f
BrandNew8787 <80979158+BrandNew8787@users.noreply.github.com>
2024-05-10 00:09:48 -0700

    added funtionality to add a recipe from the recipe parser. Currently only works with one recipe and the coroutine still does not execute all of the code

--- app/src/main/java/com/example/grocerez/ui/recipes/RecipeParsingFragment.kt
+++ app/src/main/java/com/example/grocerez/ui/recipes/RecipeParsingFragment.kt
@@ -1,6 +1,5 @@
 package com.example.grocerez.ui.recipes
 // Added to implement SocketHandler function
-import com.example.grocerez.SocketHandler
 
 
 import android.os.Bundle
@@ -9,10 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.example.grocerez.SocketHandler
+import com.example.grocerez.data.RecipeRepository
+import com.example.grocerez.data.model.Category
+import com.example.grocerez.data.model.Item
+import com.example.grocerez.data.model.Recipe
+import com.example.grocerez.data.model.RecipeItem
+import com.example.grocerez.data.model.Unit
+import com.example.grocerez.database.AppDatabase
 import com.example.grocerez.databinding.RecipeParsingBinding
 import io.socket.client.Socket
-import org.json.JSONArray
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.json.JSONException
 import org.json.JSONObject
 
@@ -22,6 +32,8 @@ class RecipeParsingFragment : Fragment() {
     private val binding get() = _binding!!
     // For accessing the backend sockets
     private lateinit var mSocket: Socket
+    private lateinit var recipeViewModel: RecipesViewModel
+    private lateinit var newRecipe: Recipe
 
 
     override fun onCreateView(
@@ -33,6 +45,22 @@ class RecipeParsingFragment : Fragment() {
         SocketHandler.establishConnection()
         mSocket = SocketHandler.getSocket()
         mSocket.connect()
+
+        val appDatabase = AppDatabase.getInstance(requireContext())
+
+        recipeViewModel = ViewModelProvider(this.requireActivity(),
+            RecipesViewModel.RecipeModelFactory(
+                RecipeRepository(
+                    categoryDao = appDatabase.categoryDao(),
+                    itemDao = appDatabase.itemDao(),
+                    recipeDao = appDatabase.recipeDao(),
+                    recipeItemDao = appDatabase.recipeItemDao(),
+                    unitDao = appDatabase.unitDao()
+                )
+            )).get(RecipesViewModel::class.java)
+
+        recipeViewModel.loadRecipes()
+
         // Inflate the layout for this fragment using view binding
         _binding = RecipeParsingBinding.inflate(inflater, container, false)
         return binding.root
@@ -113,23 +141,69 @@ class RecipeParsingFragment : Fragment() {
                 // Initialize a JSONObject with the string
                 val jsonObject = JSONObject(recipeDataJson)
 
-                // Get the ingredients and format them as a list
-                val ingredientsJsonArray = jsonObject.getJSONArray("ingredients")
-                val ingredientsList = StringBuilder("Ingredients:\n")
-                for (i in 0 until ingredientsJsonArray.length()) {
-                    val ingredient = ingredientsJsonArray.getJSONObject(i)
-                    val amount = ingredient.optString("Amount")
-                    val unit = ingredient.optString("Unit")
-                    val name = ingredient.optString("Name")
-                    ingredientsList.append("Amount: $amount, Unit: $unit, Name: $name\n")
-                }
-
                 // Get the instructions and format them as a list
                 val instructionsJsonArray = jsonObject.getJSONArray("instructions")
                 val instructionsList = StringBuilder("\nInstructions:\n")
                 for (i in 0 until instructionsJsonArray.length()) {
                     val step = instructionsJsonArray.optString(i)
                     instructionsList.append("Step ${i + 1}: $step\n")
+                }
+
+                val recipe = Recipe(
+                    name = "Meatballs",
+                    instruction = instructionsList.toString()
+                )
+
+
+                CoroutineScope(Dispatchers.Main).launch{
+                    recipeViewModel.addRecipes(recipe)
+                    newRecipe = recipeViewModel.findRecipeByName(recipe.name)!!
+                }
+
+                // Get the ingredients and format them as a list
+                val ingredientsJsonArray = jsonObject.getJSONArray("ingredients")
+                val ingredientsList = StringBuilder("Ingredients:\n")
+                for (i in 0 until ingredientsJsonArray.length()) {
+                    val ingredient = ingredientsJsonArray.getJSONObject(i)
+                    val amount = convertFractionToDecimal(ingredient.optString("Amount"))
+                    val unit = checkUnit(ingredient.optString("Unit"))
+                    val name = ingredient.optString("Name")
+
+                    CoroutineScope(Dispatchers.Main).launch{
+                        val category = recipeViewModel.findCategoryByName("meatball ingredients")
+                        val newCategory = Category(
+                            name = "meatball ingredients"
+                        )
+                        if (category == null){
+                            recipeViewModel.addCategory(newCategory)
+                        }
+
+                        val unitTest = recipeViewModel.findUnitByName(unit)
+                        val newUnit = Unit(
+                            name = unit
+                        )
+                        if (unitTest == null){
+                            recipeViewModel.addUnit(newUnit)
+                        }
+
+                        val item = recipeViewModel.findItemByName(name)
+                        val newItem = Item(
+                            name = name,
+                            unitName = unit,
+                            category = "meatball ingredients",
+                            useRate = 0.0f)
+                        if (item == null){
+                            recipeViewModel.addItem(newItem)
+                        }
+                        val searchItem = recipeViewModel.findItemByName(name)
+                        val newRecipeItem = RecipeItem(
+                            amount = amount.toFloat(),
+                            itemId = searchItem!!.item_id,
+                            recipeId = newRecipe.recipeId,
+                        )
+                        recipeViewModel.addRecipeItems(newRecipeItem)
+                    }
+                    ingredientsList.append("Amount: $amount, Unit: $unit, Name: $name\n")
                 }
 
                 // Combine ingredients and instructions into one StringBuilder
@@ -237,7 +311,40 @@ class RecipeParsingFragment : Fragment() {
 
     }
 
+    private fun convertFractionToDecimal(fraction: String): String {
+        if (!fraction.contains("/")) {
+            // If the string does not contain a fraction, return it as is
+            return fraction
+        }
 
+        val parts = fraction.split("/")
+        if (parts.size != 2) {
+            // Invalid fraction format, return original string
+            return fraction
+        }
+
+        try {
+            val numerator = parts[0].toDouble()
+            val denominator = parts[1].toDouble()
+
+            // Calculate decimal value
+            val decimalValue = numerator / denominator
+
+            // Format decimal value to two decimal places
+            return String.format("%.2f", decimalValue)
+        } catch (e: NumberFormatException) {
+            // Error occurred during conversion, return original string
+            return fraction
+        }
+    }
+
+    private fun checkUnit(unit: String): String {
+        return if (unit.isEmpty()) {
+            "count"
+        } else {
+            unit
+        }
+    }
 
 
     override fun onDestroyView() {
